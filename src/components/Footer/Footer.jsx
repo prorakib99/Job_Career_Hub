4fa9e9f1e74a85d85c08892373cf59baae9c7569
Your Name <you@example.com>
2023-12-19 01:44:39 +0600

    Footer Component Added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+    return (
+        <div className="bg-[#1A1919] py-8">
+            <div className="container mx-auto px-8">
+                <div className="flex items-center justify-between">
+                    <p className="text-white text-opacity-60 text-sm font-normal leading-snug">@2023 <span className="font-semibold">JobCareerHub</span> . All Rights Reserved</p>
+                    <p className="text-white text-opacity-60 text-sm font-normal leading-snug">Powered by <span className="font-bold">JobCareerHub</span></p>
+                </div>
+            </div>
+        </div >
+    );
+};
+
+export default Footer;

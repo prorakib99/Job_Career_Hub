caa006806ee4fe9061d9a96e9547f8e49365ebf9
Your Name <you@example.com>
2023-12-25 19:20:04 +0600

    Responsive for Mobile Device

--- src/components/AppliedJob/AppliedJob.jsx
+++ src/components/AppliedJob/AppliedJob.jsx
@@ -3,18 +3,18 @@ import locationIcon from '../../assets/location2.png';
 import moneyIcon from '../../assets/money.png';
 
 const AppliedJob = ({ job }) => {
-    const {id, logo, job_title, company_name, remote_or_onsite, location, salary} = job;
+    const { id, logo, job_title, company_name, remote_or_onsite, location, salary } = job;
     return (
-        <div className="flex justify-between items-center rounded-lg border border-gray-200">
-            <div className="flex gap-8 items-center">
-                <div className="w-60 h-60 m-7 bg-zinc-100 rounded-lg flex items-center justify-center">
-                    <img src={logo} alt="" />
-                </div>
+        <div className="grid md:grid-cols-[1fr_5fr] items-center justify-center gap-12 rounded-lg border border-gray-200 p-7">
+            <div className="w-60 h-60 bg-zinc-100 rounded-lg mx-auto flex items-center justify-center">
+                <img src={logo} alt="" />
+            </div>
+            <div className="flex md:flex-row flex-col gap-12 items-center justify-between">
                 <div>
                     <h5 className="text-zinc-700 mb-2 text-2xl font-extrabold">{job_title}</h5>
                     <p className="text-neutral-500 mb-4 text-2xl font-semibold">{company_name}</p>
                     <button className="w-[100px] px-[19px] mb-6 py-[9px] rounded border border-indigo-400 text-indigo-400 text-base font-extrabold">{remote_or_onsite}</button>
-                    <div className="flex gap-6">
+                    <div className="md:flex grid gap-6">
                         <div className="flex items-center gap-2">
                             <img src={locationIcon} alt="" />
                             <p className="text-neutral-500 text-xl font-semibold">{location}</p>
@@ -25,10 +25,13 @@ const AppliedJob = ({ job }) => {
                         </div>
                     </div>
                 </div>
+                <div className="">
+                    <Link to={`/job/${id}`}>
+                        <p className="px-[18px] py-3 bg-gradient-to-r from-indigo-400 to-violet-500 rounded text-white text-xl font-extrabold">View Details</p>
+                    </Link>
+                </div>
             </div>
-            <Link to={`/job/${id}`} className="me-12">
-                <p className="px-[18px] py-3 bg-gradient-to-r from-indigo-400 to-violet-500 rounded text-white text-xl font-extrabold">View Details</p>
-            </Link>
+
         </div>
     );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,10 +3,10 @@ const Hero = () => {
     return (
         <div className='bg-[#F8F9FF] pb-20'>
             <div className='container mx-auto px-8'>
-                <div className='grid grid-cols-2 gap-80 items-center'>
+                <div className='grid md:grid-cols-2 md:gap-80 items-center'>
                     <div className='mt-14'>
-                        <h1 className="text-zinc-900 text-[80px] mb-6 font-extrabold leading-[100px]">One Step Closer To Your <span className="text-indigo-400">Dream Job</span></h1>
-                        <p className="text-neutral-500 text-lg font-medium mb-8 font-['Manrope'] leading-[30px]">Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
+                        <h1 className="text-zinc-900 md:text-[5rem] mb-6 font-extrabold leading-[100px]">One Step Closer To Your <span className="text-indigo-400">Dream Job</span></h1>
+                        <p className="text-neutral-500 text-lg font-medium mb-8 leading-[30px]">Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
                         </p>
                         <button className="px-7 py-4 bg-gradient-to-r from-indigo-400 to-violet-500 rounded-lg text-white text-xl font-extrabold">Get Started</button>
                     </div>
